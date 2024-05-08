bccd223d43621519601d80a77fa85961284becfa
Thorsten Kummermehr <thorsten.kummermehr@microchip.com>
2024-05-08 08:06:26 +0200

    Switch to Apache license

--- tc6.py
+++ tc6.py
@@ -1,23 +1,16 @@
-#Copyright (C) 2023, Microchip Technology Inc., and its subsidiaries. All rights reserved.
+# Copyright 2024 Microchip Technology Incorporated
 #
-#The software and documentation is provided by microchip and its contributors
-#"as is" and any express, implied or statutory warranties, including, but not
-#limited to, the implied warranties of merchantability, fitness for a particular
-#purpose and non-infringement of third party intellectual property rights are
-#disclaimed to the fullest extent permitted by law. In no event shall microchip
-#or its contributors be liable for any direct, indirect, incidental, special,
-#exemplary, or consequential damages (including, but not limited to, procurement
-#of substitute goods or services; loss of use, data, or profits; or business
-#interruption) however caused and on any theory of liability, whether in contract,
-#strict liability, or tort (including negligence or otherwise) arising in any way
-#out of the use of the software and documentation, even if advised of the
-#possibility of such damage.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#Except as expressly permitted hereunder and subject to the applicable license terms
-#for any third-party software incorporated in the software and any applicable open
-#source software license terms, no license or other rights, whether express or
-#implied, are granted under any patent or other intellectual property rights of
-#Microchip or any third party.
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from enum import Enum
 
